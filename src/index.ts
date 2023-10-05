d15e22e79e75b8b6d3b96de71879925d910fe102
LeJuanTenorio <tenorio2003@gmail.com>
2023-10-05 09:22:10 -0500

    redux

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+class AppContainer extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"})
+    }
+
+    connectedCallback() {
+        this.render()
+    }
+
+    render() {
+        const dashboard = this.ownerDocument.createElement('app-dashboard');
+        this.shadowRoot?.appendChild(dashboard);
+    }
+}
+
+customElements.define('app-container', AppContainer)
---
+++ src/types/store.ts
@@ -0,0 +1,18 @@
+export type Observer = { render: () => void } & HTMLElement;
+
+export type AppState = {
+	backgroundColor: string;
+};
+
+export enum BackgroundActions {
+	'CHANGEBACKGROUND' = 'CHANGEBACKGROUND',
+}
+
+export interface ChangeBackgroundAction {
+	action: BackgroundActions.CHANGEBACKGROUND;
+	payload: 'red';
+}
+
+export type Actions = ChangeBackgroundAction;
+
+
